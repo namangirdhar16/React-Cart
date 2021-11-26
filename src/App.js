0d4dc4c2321c7e9f7a156ad9997dbe5a175318dd
namangirdhar16 <girdharnaman1611@gmail.com>
2021-11-26 12:46:32 +0530

    snapshot firebase v9 add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Cart from "./Cart";
 import NavBar from "./NavBar";
 import { db } from "./firebase-config";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc, updateDoc, doc, onSnapshot, query, deleteDoc} from "firebase/firestore";
 
 class App extends React.Component{
 
@@ -10,32 +10,43 @@ class App extends React.Component{
       super();
       this.state = {
           products: [],
-          productCount: 0
+          productCount: 0,
+          loading: true,
       }
   }
   componentDidMount() {
-           console.log("hello");
            const productsRef = collection(db, "Products")
-           const getUsers = async () => {
-             const data = await getDocs(productsRef);
-             const updatedProducts = (data.docs.map((doc) => (doc.data())));
-             
-             console.log(updatedProducts); 
-             this.setState({ products: updatedProducts }, () => {
-               console.log("done");
-             });
-           }
-           getUsers();
+           const products = query(productsRef);
+           onSnapshot(products, (snapshot) => {
+             this.setState({ products: snapshot.docs.map((doc) => ({
+               ...doc.data(), id: doc.id
+             })), loading: false})
+           })
+          //  console.log(products);
+          //  console.log("hello");
+           
+          //  const getProducts = async () => {
+          //    const data = await getDocs(productsRef);
+          //    const updatedProducts = data.docs.map((doc) => ({...doc.data(), id: doc.id}));
+          //    console.log(updatedProducts); 
+          //    this.setState({ products: updatedProducts, loading: false}, () => {
+          //      console.log("done");
+               
+          //    });
+          //  }
+          //  getProducts();
   }
-    handleIncQty = (product) => {
-      console.log("handle inc qty called!", product.title);
-      let {products, productCount} = this.state;
-      //const idx = products.findIndex((prod) => (prod.title === product.title && prod.price == prod.price));
-      productCount++;
-      //console.log(idx);
-      products[product.idx].qty += 1;
-      this.setState({...products, productCount});
-
+    handleIncQty = async (product) => {
+      // console.log("handle inc qty called!", product.title);
+      // let {products, productCount} = this.state;
+      // //const idx = products.findIndex((prod) => (prod.title === product.title && prod.price == prod.price));
+      // productCount++;
+      // //console.log(idx);
+      // products[product.idx].qty += 1;
+      // this.setState({...products, productCount});
+      const newProduct = doc(db, "Products", product.id);
+      const newFields = { qty : product.qty + 1};
+      await updateDoc(newProduct, newFields);
   } 
     handleDecQty = (product) => {
         let {products, productCount} = this.state;
@@ -57,11 +68,20 @@ class App extends React.Component{
       })
       return total;
     }
-    
+    addProduct = async () => {
+      console.log("hello");
+      const productsRef = collection(db, "Products")
+        await addDoc(productsRef, { title: "glass", qty: 1, price: 10000});
+    }
+    updateProduct = async() => {
+       
+    }
   render() {
       return (
         <div className="App">
           <NavBar productCount = {this.state.productCount} />
+          <button onClick = {this.addProduct}>Add a product</button>
+          {this.state.loading && <h1> loading...</h1>}
           <Cart products = {this.state.products} handleDelQty = {this.handleDelQty} handleIncQty = {this.handleIncQty} handleDecQty = {this.handleDecQty} />
           <div style = {styles.totalVal}>Total: {this.getTotalVal()}</div>
         </div>
--- src/Cart.js
+++ src/Cart.js
@@ -9,9 +9,9 @@ const Cart = (props) => {
     console.log(products);
     return (
         <div className = "cart">
-            {products.map(({qty, title, img, price}, idx) => 
+            {products.map(({qty, title, img, price, id}, idx) => 
             
-                <CartItem key = {idx} idx = {idx} qty = {qty} title = {title} img = {img} price = {price} onIncQty = {props.handleIncQty} onDecQty = {props.handleDecQty} onDelQty = {props.handleDelQty}/>)}
+                <CartItem key = {idx} idx = {idx} id = {id} qty = {qty} title = {title} img = {img} price = {price} onIncQty = {props.handleIncQty} onDecQty = {props.handleDecQty} onDelQty = {props.handleDelQty}/>)}
         </div>
     )
     
